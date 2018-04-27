f83392c9e6c48b59779513cafa7d7ba87e41d24d
lisea <lihai@yuanlaihuyu.com>
2018-04-27 18:33:55 +0800

    fix init

--- init.go
+++ init.go
@@ -3,18 +3,19 @@ package umfsdk
 import "github.com/cnlisea/crypto"
 
 var (
-	requestUrl string // 请求url
-	privateKey []byte // 私钥
-	publicKey  []byte // 公钥
+	requestUrl string                                                                                                                                                                                                                                                                                       // 请求url
+	privateKey []byte                                                                                                                                                                                                                                                                                       // 私钥
+	publicKey  = []byte(`-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGWAu4p6aK1SiQqNKT1nTgYwA8
+cz0Rde9fRtmLJAx1QxLqrerAUVl/uuXV7NQFSkTipouo3cwEEpae89267AeLJBzK
+PbKnUID6JYGbwnq7CiRR4E244zcgqE8jo8DnkbH3KkiWonoUMD1uHy6TUFv5W7zr
+haz/E59MVmbzrp1TwwIDAQAB
+-----END PUBLIC KEY-----
+`) // 公钥
 )
 
-func Init(url string, publicKeyPath string, privateKeyPath string) error {
+func Init(url string, privateKeyPath string) error {
 	var err error
-
-	publicKey, err = ReadFileAll(publicKeyPath)
-	if err != nil {
-		return err
-	}
 
 	privateKey, err = ReadFileAll(privateKeyPath)
 	if err != nil {
