198abf1613643f6739c051d3d81c4f199e82aa03
lisea <lihai@yuanlaihuyu.com>
2018-04-27 15:47:14 +0800

    add config struct

---
+++ config.go
@@ -0,0 +1,16 @@
+package umfsdk
+
+import "time"
+
+type Config struct {
+	MerId     string    // 商户编号
+	RetUrl    string    // 同步通知地址
+	NotifyUrl string    // 异步通知地址
+	GoodsId   string    // 商品号
+	GoodsInfo string    // 商品描述信息
+	OrderId   string    // 订单号
+	Amount    int64     // 金额, 单位分
+	MerData   time.Time // 订单日期
+	UserIp    string    // 用户IP
+	MerPriv   string    // 私有域
+}
